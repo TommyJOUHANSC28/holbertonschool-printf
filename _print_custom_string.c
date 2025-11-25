61b0f34da6d223503b82984f0e4438b4ad27e207
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-25 22:32:19 +0000

    function that string S  to non printable characters (0 < ASCII value < 32 or >= 127)

---
+++ _print_custom_string.c
@@ -0,0 +1,34 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * print_custom_string - prints a string with non-printable chars as \xHH
+ * @arg_list: argument list containing the string
+ *
+ * Return: number of characters printed
+ */
+int print_custom_string(va_list arg_list)
+{
+char *s = va_arg(arg_list, char *);
+int count = 0;
+if (s == NULL)
+s = "(null)";
+while (*s)
+{
+unsigned char c = (unsigned char)*s;
+if (c >= 32 && c < 127) /* imprimable */
+{
+count += write(1, &c, 1);
+}
+else
+{
+char hex[4];
+hex[0] = '\\';
+hex[1] = 'x';
+hex[2] = "0123456789ABCDEF"[c >> 4];
+hex[3] = "0123456789ABCDEF"[c & 0xF];
+count += write(1, hex, 4);
+}
+s++;
+}
+return count;
+}
