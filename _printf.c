3e2eaed5b6c23cbdeaf875bffdcee5a9fff391a3
maxence30 <henous.maxence@outlook.fr>
2025-11-26 14:57:31 +0000

    ok

--- _printf.c
+++ _printf.c
@@ -33,3 +33,39 @@ print_all = recording_error(format, f_list, arg_list);
 va_end(arg_list);
 return (print_all);
 }
+int _printf(const char *format, ...)
+{
+    int print_all = 0;
+    char buffer[1024];       // buffer local
+    int buf_index = 0;       // position actuelle dans le buffer
+
+    conver_t f_list[] = {
+        {"c", print_char},
+        {"s", print_string},
+        {"%", print_percent},
+        {"d", print_integer},
+        {"i", print_integer},
+        {"u", unsigned_integer},
+        {"b", print_binary},
+        {"r", print_reversed},
+        {"o", handle_octal},
+        {"x", handle_hex},
+        {"X", handle_HEX},
+        {NULL, NULL}
+    };
+
+    va_list arg_list;
+    if (format == NULL)
+        return (-1);
+    va_start(arg_list, format);
+
+    // Appelle une version modifiée de recording_error qui prend buffer et buf_index
+    print_all = recording_error(format, f_list, arg_list, buffer, &buf_index);
+
+    // Écrire tout ce qui reste dans le buffer
+    if (buf_index > 0)
+        write(1, buffer, buf_index);
+
+    va_end(arg_list);
+    return (print_all);
+}
