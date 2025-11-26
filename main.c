f716081fd7e66b9fd3f4384f27a4db98e6fe768d
maxence30 <henous.maxence@outlook.fr>
2025-11-26 15:01:37 +0000

    main.c test

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
 #include "main.h"
 
+/**
+ * main - test _printf pour tous les cas demandés
+ *
+ * Return: 0
+ */
 int main(void)
 {
-    _printf("Test : %u %o %x %X\n", 123, 123, 3055, 3055);
+    unsigned int u_val = 123;
+    unsigned int o_val = 123;
+    unsigned int x_val = 3055;
+    unsigned int X_val = 3055;
+
+    _printf("Unsigned: %u\n", u_val);
+    _printf("Octal   : %o\n", o_val);
+    _printf("Hex     : %x\n", x_val);
+    _printf("HEX     : %X\n", X_val);
+
+    _printf("Mix     : u=%u, o=%o, x=%x, X=%X\n",
+            u_val, o_val, x_val, X_val);
+
     return 0;
 }
