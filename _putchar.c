16b74c5b2bfc78bae4a8a0063093567ce460be51
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-26 21:31:39 +0000

    rename function _putchar for print a charactere

--- _putchar.c
+++ _putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _write_char - writes the character c to stdout
+ * _putchar - writes the character c to stdout
  * @c: The character to print
  *
  * Return: On success 1.
  */
-int _write_char(char c)
+int _putchar(char c)
 {
 return (write(1, &c, 1));
 }
