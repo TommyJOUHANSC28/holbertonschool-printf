07d710e6bc70fcc397694606fc7ca0cc4b3a0481
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-28 13:15:24 +0000

    function printf

--- _printf.c
+++ _printf.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdio.h>
-
+#include <unistd.h>
 /**
  * _printf - Produces output according to a format
  * @format: format string containing directives
@@ -9,7 +9,7 @@
  */
 int _printf(const char *format, ...)
 {
-    int i = 0, printed_chars = 0;
+    int i = 0, counter = 0;
     va_list args;
 
     if (format == NULL)
@@ -29,7 +29,7 @@ int _printf(const char *format, ...)
             {
                 char c = va_arg(args, int);
                 write(1, &c, 1);
-                printed_chars++;
+                counter++;
             }
             else if (format[i] == 's')
             {
@@ -40,13 +40,13 @@ int _printf(const char *format, ...)
                 {
                     write(1, s, 1);
                     s++;
-                    printed_chars++;
+                    counter++;
                 }
             }
             else if (format[i] == '%')
             {
                 write(1, "%", 1);
-                printed_chars++;
+                counter++;
             }
             else if (format[i] == 'd' || format[i] == 'i')
             {
@@ -54,23 +54,23 @@ int _printf(const char *format, ...)
                 char buf[20];
                 int len = snprintf(buf, sizeof(buf), "%d", n);
                 write(1, buf, len);
-                printed_chars += len;
+                counter += len;
             }
             else
             {
                 write(1, "%", 1);
                 write(1, &format[i], 1);
-                printed_chars += 2;
+                counter += 2;
             }
         }
         else
         {
             write(1, &format[i], 1);
-            printed_chars++;
+            counter++;
         }
         i++;
     }
 
     va_end(args);
-    return (printed_chars);
+    return (counter);
 }
