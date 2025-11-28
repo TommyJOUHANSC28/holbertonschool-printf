d943da87a664043ab9cb42dccf571e46869cef57
Tommy JOUHANS <12119@holbertonstudents.com>
2025-11-28 12:02:24 +0000

    function printf

--- _printf.c
+++ _printf.c
@@ -9,7 +9,7 @@
  */
 int _printf(const char *format, ...)
 {
-    int i = 0, counter = 0;
+    int i = 0, printed_chars = 0;
     va_list args;
 
     if (format == NULL)
@@ -28,7 +28,8 @@ int _printf(const char *format, ...)
             if (format[i] == 'c')
             {
                 char c = va_arg(args, int);
-                counter += write(1, &c, 1);
+                write(1, &c, 1);
+                printed_chars++;
             }
             else if (format[i] == 's')
             {
@@ -37,47 +38,39 @@ int _printf(const char *format, ...)
                     s = "(null)";
                 while (*s)
                 {
-                    counter += write(1, s, 1);
+                    write(1, s, 1);
                     s++;
+                    printed_chars++;
                 }
             }
             else if (format[i] == '%')
             {
-                counter += write(1, "%", 1);
+                write(1, "%", 1);
+                printed_chars++;
             }
             else if (format[i] == 'd' || format[i] == 'i')
             {
                 int n = va_arg(args, int);
-                char buf[20]; 
-                int len = 0;
-
-                if (n < 0)
-                {
-                    counter += write(1, "-", 1);
-                    n = -n;
-                }
-                do {
-                    buf[len++] = (n % 10) + '0';
-                    n /= 10;
-                } while (n > 0);
-
-                while (len--)
-                    counter += write(1, &buf[len], 1);
+                char buf[20];
+                int len = snprintf(buf, sizeof(buf), "%d", n);
+                write(1, buf, len);
+                printed_chars += len;
             }
             else
             {
-                counter += write(1, "%", 1);
-                counter += write(1, &format[i], 1);
+                write(1, "%", 1);
+                write(1, &format[i], 1);
+                printed_chars += 2;
             }
         }
         else
         {
-            counter += write(1, &format[i], 1);
+            write(1, &format[i], 1);
+            printed_chars++;
         }
         i++;
     }
 
     va_end(args);
-    return (counter);
+    return (printed_chars);
 }
-
